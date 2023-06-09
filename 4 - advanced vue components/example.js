933d3ef134bce998b94ad37dac1adc592e1d4326
M <rebelsowl53@gmail.com>
2023-06-09 20:52:28 +0300

    w 4

---
+++ 4 - advanced vue components/example.js
@@ -0,0 +1,14 @@
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return (_openBlock(), _createElementBlock(_Fragment, null, [
+    _createElementVNode("header", null, [
+      _hoisted_1,
+      _hoisted_2,
+      _createElementVNode("div", _hoisted_3, [
+        _createVNode($setup["HelloWorld"], { msg: "You did it!" })
+      ])
+    ]),
+    _createElementVNode("main", null, [
+      _createVNode($setup["TheWelcome"])
+    ])
+  ], 64 /* STABLE_FRAGMENT */))
+}
