07f6573cd4371e52087c076094a3403fffeccb53
m <rebelsowl53@gmail.com>
2023-07-14 16:53:41 +0300

    composition api

---
+++ 18 - Composition API/src/hooks/phrase.js
@@ -0,0 +1,17 @@
+import { ref, watchEffect } from "vue";
+
+export const usePhrase = () => {
+  const phrase = ref("");
+  const reversedPhrase = ref("");
+  const num = ref("");
+
+  // watch([phrase], ([newVal, oldVal]) => {
+  //   reversedPhrase.value = phrase.value.split("").reverse().join("");
+  // });
+
+  watchEffect(() => {
+    reversedPhrase.value = phrase.value.split("").reverse().join("");
+  });
+
+  return { phrase, reversedPhrase, num };
+};
